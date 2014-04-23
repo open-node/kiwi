cad89d3aac8f3878f2db2f435b66898164f7efb0
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2014-04-23 14:29:03 +0800

    添加i18n 处理机制

---
+++ bin/read.sh
@@ -0,0 +1,4 @@
+find ../app -type f | ./read.coffee > locale/application.pot
+msgmerge -UN --no-wrap ./locale/en.po ./locale/application.pot
+msgmerge -UN --no-wrap ./locale/zh.po ./locale/application.pot
+
